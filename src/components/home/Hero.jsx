8910d8a7cb7b1c2d850f5888b5334d67b468e821
Eduardo Junior <juniorcose@gmail.com>
2025-10-07 11:48:37 -0300

    fix: adjust padding in ContainerDefault and Hero components for consistent layout

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -8,7 +8,7 @@ import SearchBar from "../search/SearchBar";
 const Hero = () => {
   return (
     <div className="relative md:h-[731px] bg-primary ">
-      <ContainerDefault className="h-full px-0 md:pl-4 lg:pr-0 gap-0 md:gap-4 flex-col-reverse md:flex-row">
+      <ContainerDefault className="h-full pl-0 pr-0 md:pl-4 lg:pr-0 gap-0 md:gap-4 flex-col-reverse md:flex-row">
         <div className="w-full max-w-full md:max-w-[638px] p-6 pb-12 md:pb-0 md:pt-40">
           <h1 className="text-[32px] md:text-5xl lg:text-[52px]  text-white mb-4 drop-shadow-lg leading-[110%] tracking-[-0.6] md:tracking-[-1px]">
             Veja a reputação de qualquer condomínio
